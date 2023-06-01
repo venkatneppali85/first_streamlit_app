ad624ea5e0048d838205f2fafc8ea5eda8cb8490
venkatneppali85 <135260201+venkatneppali85@users.noreply.github.com>
2023-06-01 23:23:14 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 import streamlit
 streamlit.title('My parents new healthy diner')
-streamlit.header('Break Fast Menu')
-streamlit.text('Idly')
-streamlit.text('Dosa')
-streamlit.text('Vada')
+streamlit.header('Breakfast Menu')
+streamlit.text('Omega 3 & Blueberry Oatmeal')
+streamlit.text('Kale, Spinach & Rocket Smoothie')
+streamlit.text('Hard-Boiled Free-Range Egg')

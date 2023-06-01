64c130000e4745766437b78622641d02156a5874
venkatneppali85 <135260201+venkatneppali85@users.noreply.github.com>
2023-06-01 23:30:18 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,10 @@
+import streamlit
+streamlit.title('My parents first diner')
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
+streamlit.text('Idly')
+streamlit.text('Dosa')
+streamlit.text('Vada')
+
 import pandas
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 streamlit.dataframe(my_fruit_list)
